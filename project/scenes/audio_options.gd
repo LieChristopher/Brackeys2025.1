6570a3f8f686a36f8fc41c7edce2060ef46438a3
JoenTNT <tntmasterjoen@gmail.com>
2025-02-22 15:04:09 +0700

    Assigned  Audio Events

--- project/scenes/audio_options.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-
-#func _on_volume_slider_drag_ended(value_changed: bool) -> void:
-	#AudioServer.set_bus_volume_db(0,linear_to_db($MarginContainer2/VBoxContainer/VolumeSlider.value))
-	#pass # Replace with function body.
--- project/scripts/audio-system/UI_AudioSettings.gd
+++ project/scripts/audio-system/UI_AudioSettings.gd
@@ -1,2 +1,30 @@
 class_name UI_AudioSettings
 extends Control
+
+@export var _master_slider: Slider = null;
+@export var _bgm_slider: Slider = null;
+@export var _sfx_slider: Slider = null;
+
+func on_master_slider_value_changed(v: float) -> void:
+	# TODO: Live audio changed.
+	pass;
+
+func on_bgm_slider_value_changed(v: float) -> void:
+	# TODO: Live audio changed.
+	pass;
+
+func on_sfx_slider_value_changed(v: float) -> void:
+	# TODO: Live audio changed.
+	pass;
+
+func _on_master_slider_drag_ended(value_changed: bool) -> void:
+	AudioServer.set_bus_volume_db(0, linear_to_db(_master_slider.value));
+
+func _on_bgm_slider_drag_ended(value_changed: bool) -> void:
+	AudioServer.set_bus_volume_db(1, linear_to_db(_bgm_slider.value));
+
+func _on_sfx_slider_drag_ended(value_changed: bool) -> void:
+	AudioServer.set_bus_volume_db(2, linear_to_db(_sfx_slider.value));
+
+#func _on_volume_slider_drag_ended(value_changed: bool) -> void:
+	#AudioServer.set_bus_volume_db(0,linear_to_db($MarginContainer2/VBoxContainer/VolumeSlider.value))
