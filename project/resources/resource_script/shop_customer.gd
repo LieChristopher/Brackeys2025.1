f89eadb7a0a19b98ea06591700cfc53f083d6eca
LieChristopher <liechristopher@gmx.com>
2025-02-22 18:39:23 +0700

    cutscene logic

--- project/resources/resource_script/shop_customer.gd
+++ project/resources/resource_script/shop_customer.gd
@@ -1,9 +1,25 @@
 extends Resource
+class_name ShopCustomer
 
-var sprite: Sprite2D
-var name: String
-var requestedPotion: RecipePotion
+@export var code: String
+@export var char: DialogicCharacter
 var moodGood: bool
+var interactionCounter: int
 
-func dialogue():
+func dialogue(is_start:bool):
+	var suffix = "START"
+	if not is_start:
+		suffix = "END"
+	
+	interactionCounter = Dialogic.VAR.get("InteractionCounter").get(code)
+	if interactionCounter > 1:
+		moodGood = Dialogic.VAR.get("MoodGood").get(code)
+		if moodGood:
+			suffix = "GoodMood_" + suffix
+		else:
+			suffix = "BadMood_" + suffix
+	
+	var timeline = (code+"_Day%02d_"+suffix)%interactionCounter
+	print(timeline)
+	Dialogic.start(timeline)
 	pass
---
+++ project/resources/resource_script/shop_customer_request.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name ShopCustomerRequest
+
+@export var customer: ShopCustomer
+@export var requestedPotion: RecipePotion
