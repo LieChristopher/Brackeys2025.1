5b01cbf43fb84e2738f5a081783234e2890deeda
JoenTNT <tntmasterjoen@gmail.com>
2025-02-24 22:49:53 +0700

    Fixed Interactions in Gameplay

--- almanac_ui.gd
+++ almanac_ui.gd
@@ -1,37 +1,35 @@
 extends Control
 
+@export var _content: Control = null;
+@export var _recipe_book: TextureRect = null;
 @export var _page_hint_txt: RichTextLabel = null;
 @export var _formatted_text: String = "";
 
-var screenHidden = true;
-var i = 1;
+# Runtime variable data.
+var screenHidden: bool = true;
+var i: int = 1;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AlmanacScreen.hide()
+	_content.hide()
 
 func _on_show_almanac_button_pressed() -> void:
-	if screenHidden:
-		$AlmanacScreen.show()
-		screenHidden = false;
-	else:
-		$AlmanacScreen.hide()
-		screenHidden = true;
+	if screenHidden: _content.show()
+	else: _content.hide()
+	screenHidden = !screenHidden;
 	_page_hint_txt.text = _formatted_text.format({
 		"c": i, "p": 4});
 
 func _on_next_pressed() -> void:
-	print("next button cicked")
-	if i < 4:
-		i+=1
-	$"AlmanacScreen/Recipe Book".texture = load("res://project/arts/internal/sprites/almanac/Alamanac-"+str(i)+".png")
+	print("next button cicked");
+	if i < 4: i += 1;
+	_recipe_book.texture = load("res://project/arts/internal/sprites/almanac/Alamanac-"+str(i)+".png")
 	_page_hint_txt.text = _formatted_text.format({
 		"c": i, "p": 4});
 
 func _on_prev_pressed() -> void:
-	print("prev button cicked")
-	if i > 1:
-		i-=1
-	$"AlmanacScreen/Recipe Book".texture = load("res://project/arts/internal/sprites/almanac/Alamanac-"+str(i)+".png")
+	print("prev button cicked");
+	if i > 1: i -= 1;
+	_recipe_book.texture = load("res://project/arts/internal/sprites/almanac/Alamanac-"+str(i)+".png")
 	_page_hint_txt.text = _formatted_text.format({
 		"c": i, "p": 4});
